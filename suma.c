85d5c2adc4cc835016464a729200d279d28fc6e3
Anthony <a.ortizchavez@icloud.com>
2025-06-03 20:57:59 +0200

    suma

---
+++ suma.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+	int main() 
+
+{
+
+	int a, b , suma;
+
+	printf("Escriba el primer numero: ");
+	scanf("%d", &a);
+
+	printf("Escriba el segundo numero: ");
+	scanf("%d", &b);
+
+	suma = a + b;
+
+	printf("La suma es %d.\n", suma);
+
+	return 0;
+
+}
+
+ 
